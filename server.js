807cfde80ddabe3359dca2cb243e77840cb17ced
apollineguyot <apollineguyot@gmail.com>
2025-11-26 12:14:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -150,6 +150,7 @@ app.post("/webhook/orders/create", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
